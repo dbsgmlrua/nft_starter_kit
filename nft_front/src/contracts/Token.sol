6f98bad831ebcc39d92f437eea5cdbf5d6630921
dbsgmlrua <dbsgmlrua@naver.com>
2021-12-29 00:26:25 +0900

    init truffle and token

---
+++ nft_front/src/contracts/Token.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Token is ERC721, ERC721Enumerable, Ownable{
+    constructor() ERC721("Simple Nft Token", "SNT") {
+        // minttoken();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "<HARD CODED YOUR BASEURL>";
+    }
+}
